4c49f3214a081a7cc851b663d21fa708f4d810f8
CannibalCake <mattsverypersonalemail@gmail.com>
2024-05-13 12:49:58 +0100

    replaced spaces with underscores in main variable

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
-import typenullsfx from "./typenullsfx";
-let yo = {
-  bomb: "/nullsfx/sfx/Bomb.wav",
-  epic: "/nullsfx/sfx/Epic.wav",
-  wasted: "/nullsfx/sfx/Wasted.wav",
-  iphone: "/nullsfx/sfx/iPhone (+Reverb).wav",
-  nani: "/nullsfx/sfx/Metal Gear Alert_ Sound Effect(MP3_160K)_1.mp3",
-  splat: "/nullsfx/sfx/Splat - Gaming Sound Effect (HD)(MP3_128K)_1.mp3",
-  teleport: "/nullsfx/sfx/Teleport Sound Effect(MP3_320K).mp3",
-  click: "/nullsfx/sfx/Mouse Click - Sound Effect (HD)(MP3_160K)_1.mp3",
-  magic: "/nullsfx/sfx/Magic.wav",
-  kwak: "/nullsfx/sfx/Duck Quack - Sound Effect (HD)(MP3_160K)_1.mp3",
-  dbz: "/nullsfx/sfx/Dragon Ball.wav",
-  censor: "/nullsfx/sfx/Censorship.wav",
-  bed: "/nullsfx/sfx/Bed Squeak.mp3",
-  angel: "/nullsfx/sfx/Angelic Voice.wav",
-  amongus: "/nullsfx/sfx/Among Us.mp3",
-  audience: "/nullsfx/sfx/Audience.wav",
-  tititik: "/nullsfx/sfx/Tik Tik Sound Effect By Techno.mp3",
+import typenullsfx from "./typenullsfx.js";
+let yo:{[key:string]:string} = {
+    bomb: "./sfx/Bomb.wav",
+    epic: "./sfx/Epic.wav",
+    wasted: "./sfx/Wasted.wav",
+    iphone: "./sfx/iPhone_(+Reverb).wav",
+    nani: "./sfx/Metal_Gear_Alert__Sound_Effect(MP3_160K)_1.mp3",
+    splat: "./sfx/Splat_-_Gaming_Sound_Effect_(HD)(MP3_128K)_1.mp3",
+    teleport: "./sfx/Teleport_Sound_Effect(MP3_320K).mp3",
+    click: "./sfx/Mouse_Click_-_Sound_Effect_(HD)(MP3_160K)_1.mp3",
+    magic: "./sfx/Magic.wav",
+    kwak: "./sfx/Duck_Quack_-_Sound_Effect_(HD)(MP3_160K)_1.mp3",
+    dbz: "./sfx/Dragon_Ball.wav",
+    censor: "./sfx/Censorship.wav",
+    bed: "./sfx/Bed_Squeak.mp3",
+    angel: "./sfx/Angelic_Voice.wav",
+    amongus: "./sfx/Among_Us.mp3",
+    audience: "./sfx/Audience.wav",
+    tititik: "./sfx/Tik_Tik_Sound_Effect_By_Techno.mp3"
 };
 
 let mysfxer = new typenullsfx(yo);
